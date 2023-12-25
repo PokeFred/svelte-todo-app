13bf28d1edadb13aa1b23add08da0c71be578870
PokeFred <cedric.paelmke@gmx.de>
2023-12-25 15:33:26 +0100

    add svelte-awesome

--- src/app.svelte
+++ src/app.svelte
@@ -1,1 +1,6 @@
-<div>Hello World</div>
+<script lang="ts">
+    import Icon from "svelte-awesome"
+    import { faSmile } from "@fortawesome/free-solid-svg-icons/faSmile"
+</script>
+
+<div>Hello World <Icon data={faSmile} /></div>
