fcfa493e21e0d107408ff5c3add6dfc1e6ba70bf
PokeFred <cedric.paelmke@gmx.de>
2023-12-25 15:23:00 +0100

    add prettier

---
+++ prettier.config.js
@@ -0,0 +1,32 @@
+/** @type {import("prettier").Config} */
+const config = {
+    tabWidth: 4,
+    useTabs: false,
+    bracketSpacing: true,
+    bracketSameLine: false,
+    overrides: [
+        {
+            files: ['*.js', '*.ts', '*.d.ts'],
+            options: {
+                semi: false,
+                singleQuote: true,
+                quoteProps: 'preserve',
+                trailingComma: 'es5',
+                arrowParens: 'always',
+            },
+        },
+        {
+            files: ['*.svelte'],
+            options: {
+                parser: 'svelte',
+                svelteSortOrder: 'options-scripts-markup-styles',
+                svelteStrictMode: false,
+                svelteAllowShorthand: false,
+                svelteIndentScriptAndStyle: true,
+            },
+        },
+    ],
+    plugins: ['prettier-plugin-svelte'],
+}
+
+export default config
